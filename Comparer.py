cf61154464a54800c0c272d08b24ae19420bbfc9
dominictdavies <dominictdavies@gmail.com>
2023-05-03 01:53:30 +0800

    comparer to Comparer

--- Comparer.py
+++ Comparer.py
@@ -80,7 +80,7 @@ def main(input_file, output_file):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python comparer.py <input_file> <output_file>")
+        print("Usage: python Comparer.py <input_file> <output_file>")
         sys.exit(1)
 
     input_file = sys.argv[1]
